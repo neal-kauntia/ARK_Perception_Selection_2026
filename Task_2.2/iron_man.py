d95e7c028ab2e7623cddca795a589155bd5f34c4
neal-kauntia <nealkauntia@gmail.com>
2026-03-02 13:15:19 +0530

    task 3 start

--- Task_2.2/iron_man.py
+++ Task_2.2/iron_man.py
@@ -7,7 +7,7 @@ gray= cv.cvtColor(img,cv.COLOR_BGR2GRAY) #Converting to grayscale img
 medfil = cv.medianBlur(gray,3)
 # cv.imshow("denoised",medfil)
 
-kernel = cv.getStructuringElement(cv.MORPH_RECT,(1,1))
+kernel = cv.getStructuringElement(cv.MORPH_RECT,(2,2))
 morphopen= cv.morphologyEx(medfil,cv.MORPH_OPEN,kernel)
 cv.imwrite("iron_man_final.png",morphopen)
 cv.imshow("result",morphopen)
--- Task_2.2/scenary.py
+++ Task_2.2/scenary.py
@@ -4,12 +4,13 @@ img = cv.imread("noisy.jpg")
 cv.imshow("img",img)
 gaus= cv.GaussianBlur(img,(3,3),0.5)
 cv.imshow("gaus",gaus)
-result = cv.fastNlMeansDenoisingColored(gaus, None,h=13,hColor= 13, templateWindowSize=3,searchWindowSize= 8)
+result = cv.fastNlMeansDenoisingColored(gaus, None,h=15,hColor= 15, templateWindowSize=3,searchWindowSize= 10)
+cv.imshow("result",result)
+kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(2,2))
+morph = cv.morphologyEx(result,cv.MORPH_OPEN,kernel)
 
-
-
-cv.imwrite("scenary_final.png",result)
-cv.imshow("final",result)
+cv.imwrite("scenary_final.png",morph)
+cv.imshow("final",morph)
 
 
 cv.waitKey(0)
