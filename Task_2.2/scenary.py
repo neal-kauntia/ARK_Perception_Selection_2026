7c4aeb099e5c20fc62ecd1e96a5716804890af81
neal-kauntia <nealkauntia@gmail.com>
2026-03-02 13:17:40 +0530

    t

--- Task_2.2/scenary.py
+++ Task_2.2/scenary.py
@@ -3,9 +3,9 @@ import cv2 as cv
 img = cv.imread("noisy.jpg")
 cv.imshow("img",img)
 gaus= cv.GaussianBlur(img,(3,3),0.5)
-cv.imshow("gaus",gaus)
+# cv.imshow("gaus",gaus)
 result = cv.fastNlMeansDenoisingColored(gaus, None,h=15,hColor= 15, templateWindowSize=3,searchWindowSize= 10)
-cv.imshow("result",result)
+# cv.imshow("result",result)
 kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(2,2))
 morph = cv.morphologyEx(result,cv.MORPH_OPEN,kernel)
 
